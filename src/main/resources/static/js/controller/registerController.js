236c9db159a32dd43bea5ad2a82f2ad1dbc2ef4b
nhutvo <minhut3818@gmail.com>
2017-03-08 16:54:08 -0500

    compare password vs comfirm password

--- src/main/resources/static/js/controller/registerController.js
+++ src/main/resources/static/js/controller/registerController.js
@@ -30,3 +30,23 @@ app.controller('registerController', function($scope, $routeParams) {
 		.error(function(data, status, headers, config){});
 	}
 });
+var compareTo = function() {
+    return {
+        require: "ngModel",
+        scope: {
+            otherModelValue: "=compareTo"
+        },
+        link: function(scope, element, attributes, ngModel) {
+             
+            ngModel.$validators.compareTo = function(modelValue) {
+                return modelValue == scope.otherModelValue;
+            };
+ 
+            scope.$watch("otherModelValue", function() {
+                ngModel.$validate();
+            });
+        }
+    };
+};
+ 
+app.directive("compareTo", compareTo);
