7aec3bf3192d1e60261cc5df6119f2ab5df13199
Tan To <tantt3746@gmail.com>
2017-03-11 12:28:32 +0700

    Add user, add book

--- 2. Source code/src/main/java/com/team3/LMS/dto/Book.java
+++ 2. Source code/src/main/java/com/team3/LMS/dto/Book.java
@@ -1,10 +1,9 @@
 package com.team3.LMS.dto;
 
 import java.io.Serializable;
-import java.time.Year;
-import java.util.Date;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,8 +12,8 @@ import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Cascade;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -37,9 +36,9 @@ public class Book implements Serializable {
 	@Column(name = "importance")
 	private int importance;
 
-	@Temporal(TemporalType.DATE)
+	// @Temporal(TemporalType.DATE)
 	@Column(name = "publishing_year")
-	private Date publishingYear;
+	private int publishingYear;
 
 	@Column(name = "short_description")
 	private String shortDescription;
@@ -50,7 +49,8 @@ public class Book implements Serializable {
 	private byte validStatus;
 
 	// bi-directional many-to-many association to AuthorDetail
-	@ManyToMany(mappedBy = "books")
+	@ManyToMany(mappedBy = "books", cascade = CascadeType.REMOVE)
+	@Cascade(org.hibernate.annotations.CascadeType.DELETE_ORPHAN)
 	private List<AuthorDetail> authorDetails;
 
 	// bi-directional many-to-one association to BookCategoryDetail
@@ -64,7 +64,9 @@ public class Book implements Serializable {
 	private PublisherDetail publisherDetail;
 
 	// bi-directional many-to-one association to TicketBookUser
-	@OneToMany(mappedBy = "book")
+	@OneToMany(mappedBy = "book", cascade = CascadeType.REMOVE)
+
+	@Cascade(org.hibernate.annotations.CascadeType.DELETE_ORPHAN)
 	private List<TicketBookUser> ticketBookUsers;
 
 	public Book() {
@@ -102,11 +104,11 @@ public class Book implements Serializable {
 		this.importance = importance;
 	}
 
-	public Date getPublishingYear() {
+	public int getPublishingYear() {
 		return this.publishingYear;
 	}
 
-	public void setPublishingYear(Date publishingYear) {
+	public void setPublishingYear(int publishingYear) {
 		this.publishingYear = publishingYear;
 	}
 
