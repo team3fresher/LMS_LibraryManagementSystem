cbc14cb50684ab070d206d2fd950117eaa6b6573
Tan To <tantt3746@gmail.com>
2017-03-14 07:14:01 +0700

    Edit Book, BookCategoryDetail

--- 2. Source code/src/main/java/com/team3/LMS/controller/BookController.java
+++ 2. Source code/src/main/java/com/team3/LMS/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.team3.LMS.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,39 +14,53 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.team3.LMS.dto.Book;
+import com.team3.LMS.dto.BookCategoryDetail;
 import com.team3.LMS.service.BookService;
 
 @Controller
+@RequestMapping(value = "/book")
 public class BookController {
 
 	@Autowired
 	BookService service;
 
-	@RequestMapping(value = "/book/list", method = RequestMethod.GET)
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Book> getBookList() {
 		return service.getBookList();
 	}
 
-	@RequestMapping(value = "/book/findAll", method = RequestMethod.GET)
+	@RequestMapping(value = "/findAll", method = RequestMethod.GET)
 	@ResponseBody
 	public Page<Book> findAll(Pageable pageable) {
 		Page<Book> books = service.findAll(pageable);
 		return books;
 	}
 
-	@RequestMapping(value = "/book/add", method = RequestMethod.POST)
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	@ResponseBody
 	public void addBook(@RequestBody Book book) {
 		service.addBook(book);
 	}
 	
-	@RequestMapping("/book/remove/{id}")
+	@RequestMapping(value = "/getCategory", method = RequestMethod.GET)
+	@ResponseBody
+	public List<BookCategoryDetail> getCategory() {
+		return service.getBookCategoryDetail();
+	}
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.POST)
+	@ResponseBody
+	public void editBook(@RequestBody Map<String, Object> map, @PathVariable String id) {
+		service.editBook(map, id);
+	}
+	
+	@RequestMapping("/remove/{id}")
 	public void removeBook(@PathVariable String id) {
 		service.removeBook(id);
 	}
 	
-	@RequestMapping(value = "/book/get/{isbn}", method = RequestMethod.GET)
+	@RequestMapping(value = "/get/{isbn}", method = RequestMethod.GET)
 	@ResponseBody
 	public Book getBook(@PathVariable String isbn) {
 		return service.getBook(isbn);
