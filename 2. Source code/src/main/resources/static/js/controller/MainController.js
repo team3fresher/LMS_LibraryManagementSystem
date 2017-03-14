76748d521ec5fde6a20f6c7a85803a1f2d7d0b84
nhutvo <minhut3818@gmail.com>
2017-03-14 14:59:11 -0400

    update index
    Signed-off-by: nhutvo <minhut3818@gmail.com>

--- 2. Source code/src/main/resources/static/js/controller/MainController.js
+++ 2. Source code/src/main/resources/static/js/controller/MainController.js
@@ -1,7 +1,17 @@
 var app = angular.module('myApp');
 
-app.controller('MainController', function($scope, $routeParams) {
+app.controller('MainController', function($scope, $http, $routeParams) {
+	getData();
+	function getData() { 
+		$http({
+			method: 'get',
+			url: "http://localhost:9000/LMS/book/list"
+		}).success(function(data, status, headers, config){
+			$scope.books = data;
 
-	$scope.text = "MainController";
+		})
+		.error(function(data, status, headers, config){});
+	}
+
 
 });
--- 2. Source code/src/main/resources/static/js/controller/bookDetailController.js
+++ 2. Source code/src/main/resources/static/js/controller/bookDetailController.js
@@ -1,7 +1,18 @@
 var app = angular.module('myApp');
 
-app.controller('bookDetailController', function($scope, $routeParams) {
-
-	$scope.text = "bookDetailController";
+app.controller('bookDetailController', function($scope,$http, $routeParams) {
+	$http({
+		method: 'get',
+		url: "http://localhost:9000/LMS/book/get/"+$routeParams.id
+	}).success(function(data) {
+		$scope.isbn=data.isbn,
+		$scope.amount=data.amount,
+		$scope.brwTcktNber=data.brwTcktNber,
+		$scope.importance=data.importance,
+		$scope.publishingYear=data.publishingYear,
+		$scope.shortDescription=data.shortDescription,
+		$scope.title=data.title,
+		$scope.validStatus=data.validStatus
+	})
 
 });
