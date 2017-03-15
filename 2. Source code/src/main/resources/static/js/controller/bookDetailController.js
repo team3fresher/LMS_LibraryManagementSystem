3b85d76c4bcd31de10829cf162860cfdd4c29619
nhutvo <minhut3818@gmail.com>
2017-03-15 17:57:53 -0400

    update index, book detail
    Signed-off-by: nhutvo <minhut3818@gmail.com>

--- 2. Source code/src/main/resources/static/js/controller/bookDetailController.js
+++ 2. Source code/src/main/resources/static/js/controller/bookDetailController.js
@@ -1,18 +1,20 @@
 var app = angular.module('myApp');
-
-app.controller('bookDetailController', function($scope,$http, $routeParams) {
+app.controller('bookDetailController', function($scope,$http, $routeParams, $rootScope) {
 	$http({
 		method: 'get',
 		url: "http://localhost:9000/LMS/book/get/"+$routeParams.id
 	}).success(function(data) {
-		$scope.book.isbn=data.isbn,
-		$scope.book.amount=data.amount,
-		$scope.book.brwTcktNber=data.brwTcktNber,
-		$scope.book.importance=data.importance,
-		$scope.book.publishingYear=data.publishingYear,
-		$scope.book.shortDescription=data.shortDescription,
-		$scope.book.title=data.title,
-		$scope.book.validStatus=data.validStatus
+		$scope.bookDetail=data;
+		$scope.index=$scope.bookDetail.bookCategoryDetail.categoryId;
+		getData();
 	})
-
+	function getData() { 
+		$http({
+			method: 'get',
+			url: "http://localhost:9000/LMS/category/get/"+$scope.index
+		}).success(function(dataBook){
+			$scope.bookRelation = dataBook;	
+		})
+		.error(function(data, status, headers, config){});
+	}
 });
